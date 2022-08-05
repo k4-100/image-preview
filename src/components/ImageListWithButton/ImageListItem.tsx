9d99ac86ea67421d6c5e2926271aac69290510e1
k4-100 <k4-100@protonmail.com>
2022-08-05 21:56:43 +0200

    this time with said component

---
+++ src/components/ImageListWithButton/ImageListItem.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const ImageListButtonItem: React.FC = () => {
+  return <Button>./file-1.jpg</Button>;
+};
+
+export default ImageListButtonItem;
