18def1d7836c6b55e2b1130c3890f7a57471649c
k4-100 <k4-100@protonmail.com>
2022-08-06 11:49:18 +0200

    finised basic look

--- src/components/PreviedImage/index.tsx
+++ src/components/PreviedImage/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import { Box } from "@mui/material";
 const PreviedImage: React.FC = () => {
-  return <div></div>;
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        marginLeft: "auto",
+        width: "fit-content",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src="logo192.png"
+        alt="image"
+        style={{
+          display: "block",
+          marginRight: "50px",
+        }}
+      />
+    </Box>
+  );
 };
 
 export default PreviedImage;
