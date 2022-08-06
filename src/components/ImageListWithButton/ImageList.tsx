2f5d96659042914bc0de6aa77bb1fe6fa0b53ee3
k4-100 <k4-100@protonmail.com>
2022-08-06 10:54:06 +0200

    Stopped working on StyledImageListItem

--- src/components/ImageListWithButton/ImageList.tsx
+++ src/components/ImageListWithButton/ImageList.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import React from "react";
 import ImageListButtonItem from "./ImageListItem";
+// import StyledImageListItem from "./StyledImageListItem";
 
 const ImageList: React.FC = () => {
   return (
     <Box>
       <ImageListButtonItem />
+      {/* <StyledImageListItem /> */}
       <ImageListButtonItem />
       <ImageListButtonItem />
       <ImageListButtonItem />
---
+++ src/components/ImageListWithButton/StyledImageListItem.tsx
@@ -0,0 +1,12 @@
+import { styled } from "@mui/system";
+
+import ImageListItem from "./ImageListItem";
+
+const StyledImageListItem = styled(ImageListItem)({
+  color: "green",
+  backgroundColor: "red",
+  padding: 8,
+  borderRadius: 4,
+});
+
+export default StyledImageListItem;
