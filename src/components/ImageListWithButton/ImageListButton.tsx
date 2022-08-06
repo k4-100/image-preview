c9fd5310933df064d713ec8818ace8ddf5e93900
k4-100 <k4-100@protonmail.com>
2022-08-06 12:23:24 +0200

    Replaced button with labeled input in ImageListButton for file download

--- src/components/ImageListWithButton/ImageListButton.tsx
+++ src/components/ImageListWithButton/ImageListButton.tsx
@@ -1,18 +1,33 @@
-import { Button } from "@mui/material";
+import { Input, InputLabel } from "@mui/material";
 import React from "react";
 
 const ImageListButton: React.FC = () => {
   return (
-    <Button
-      variant="contained"
-      sx={{
-        width: 1,
-        borderTopLeftRadius: 0,
-        borderTopRightRadius: 0,
-      }}
-    >
-      + Upload New Image
-    </Button>
+    <>
+      <InputLabel
+        htmlFor="upload"
+        sx={{
+          p: 1,
+          color: "white",
+          backgroundColor: "primary.main",
+          borderRadius: "0 0 4px 4px",
+        }}
+      >
+        + Upload New Image
+      </InputLabel>
+      <Input
+        id="upload"
+        type="file"
+        sx={{
+          zIndex: "-1",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: 0,
+          height: 0,
+        }}
+      />
+    </>
   );
 };
 
