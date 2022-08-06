ac6f853904f3e2088a6c76d17fd7b6ff609845ca
k4-100 <k4-100@protonmail.com>
2022-08-06 11:28:33 +0200

    Changed background for the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ImageListWithButton from "./components/ImageListWithButton";
 import PreviedImage from "./components/PreviedImage";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "purple",
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <ImageListWithButton />
-      <PreviedImage />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <CssBaseline />
+        <ImageListWithButton />
+        <PreviedImage />
+      </div>
+    </ThemeProvider>
   );
 };
 
