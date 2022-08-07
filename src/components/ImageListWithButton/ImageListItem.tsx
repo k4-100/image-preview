ff80a30183c830165485feabf0313ede92457eb6
k4-100 <k4-100@protonmail.com>
2022-08-07 14:48:53 +0200

    Changed capitalisation of letters to initial

--- src/components/ImageListWithButton/ImageListItem.tsx
+++ src/components/ImageListWithButton/ImageListItem.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { useGlobalContext } from "../../context";
 
 type Props = {
   handleClick: Function;
@@ -7,16 +8,20 @@ type Props = {
 };
 
 const ImageListItem: React.FC<Props> = ({ handleClick, index }) => {
+  const { data } = useGlobalContext();
+  const file = data.files.find((fl, i) => i === index);
   return (
     <Button
       sx={{
         display: "block",
         width: "100%",
         textAlign: "left",
+        textTransform: "initial",
       }}
       onClick={() => handleClick(index)}
     >
-      File {index + 1}
+      {" "}
+      ./{file?.name}
     </Button>
   );
 };
